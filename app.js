91b66e8ce06a27f10f64e08a29d7207f9bd4e62e
TerryHu82 <hubin_82@126.com>
2022-11-05 12:31:54 +0800

    test

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("hello world!");
+const express = require('express');
+const app = express();
+app.get('/',function(req,res){
+    res.send('Hello, world\n');
+});
+
+app.listen(8080, function(){
+    console.log('example app listening on port 8080');
+});
